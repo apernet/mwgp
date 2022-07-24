1484952f0a7ce710f1cb5f139049b21f7cab2c45
Haruue Icymoon <i@haruue.moe>
2022-07-24 15:45:56 +0800

    chore(obfs): only modify hash result of wg header

--- obfs.go
+++ obfs.go
@@ -23,8 +23,9 @@ import (
 //      we generate a 16-byte random bytes, attach it to the end of message,
 //      and set packet[1] to 0x01.
 // A.2. Use the end 16-bytes of message as nonce to obfuscate the message.
-// A.3. Generate the XOR patterns with MODIFIED_XXHASH64(NONCE+N*USERKEYHASH),
-//      where (N-1) is the index of 8-bytes in the packet data.
+// A.3. Generate the XOR patterns with XXHASH64(NONCE+N*USERKEYHASH),
+//      where (N-1) is the index of 8-bytes in the packet data,
+//      but as for N=1, we use MODIFIED_XXHASH64() instead of XXHASH64()
 // A.4. Obfuscate the packet data with XOR patterns.
 //      For MessageInitiation, MessageResponse, and MessageCookieReply, we only obfuscate their origin length.
 //      For MessageTransport, we only obfuscate the first 16-bytes.
@@ -50,6 +51,7 @@ const (
 	kObfuscateRandomSuffixMaxLength  = 384
 	kObfuscateSuffixAsNonceMinLength = 256
 	kObfuscateNonceLength            = 16
+	kObfuscateXORKeyLength           = 8
 
 	kMessageInitiationTypeMAC2Offset = 132
 	kMessageResponseTypeMAC2Offset   = 76
@@ -128,14 +130,17 @@ func (o *WireGuardObfuscator) Obfuscate(packet *Packet) {
 	var nonce [kObfuscateNonceLength]byte
 	copy(nonce[:], packet.Data[packet.Length-kObfuscateNonceLength:])
 
-	var digest ModifiedXXHashDigest
+	var digest xxhash.Digest
 	digest.Reset()
 	_, _ = digest.Write(nonce[:])
-	for i := 0; i < obfsPartLength; i += 8 {
+	for i := 0; i < obfsPartLength; i += kObfuscateXORKeyLength {
 		_, _ = digest.Write(o.userKeyHash[:])
-		var xorKey [8]byte
+		var xorKey [kObfuscateXORKeyLength]byte
 		digest.Sum(xorKey[:0])
-		for j := i; j < i+8 && j < obfsPartLength; j++ {
+		if i == 0 {
+			o.modifyHashMaskForWireGuardHeaderConflict(xorKey[:])
+		}
+		for j := i; j < i+kObfuscateXORKeyLength && j < obfsPartLength; j++ {
 			packet.Data[j] ^= xorKey[j-i]
 		}
 	}
@@ -157,15 +162,16 @@ func (o *WireGuardObfuscator) Deobfuscate(packet *Packet) {
 	var nonce [kObfuscateNonceLength]byte
 	copy(nonce[:], packet.Data[packet.Length-kObfuscateNonceLength:])
 
-	var digest ModifiedXXHashDigest
+	var digest xxhash.Digest
 	digest.Reset()
 	_, _ = digest.Write(nonce[:])
 
 	// decode first 8 bytes for message type
 	_, _ = digest.Write(o.userKeyHash[:])
-	var xorKey [8]byte
+	var xorKey [kObfuscateXORKeyLength]byte
 	digest.Sum(xorKey[:0])
-	for i := 0; i < 8; i++ {
+	o.modifyHashMaskForWireGuardHeaderConflict(xorKey[:])
+	for i := 0; i < kObfuscateXORKeyLength; i++ {
 		packet.Data[i] ^= xorKey[i]
 	}
 
@@ -209,10 +215,10 @@ func (o *WireGuardObfuscator) Deobfuscate(packet *Packet) {
 	}
 
 	// decode the rest
-	for i := 8; i < obfsPartLength; i += 8 {
+	for i := kObfuscateXORKeyLength; i < obfsPartLength; i += kObfuscateXORKeyLength {
 		_, _ = digest.Write(o.userKeyHash[:])
 		digest.Sum(xorKey[:0])
-		for j := i; j < i+8 && j < obfsPartLength; j++ {
+		for j := i; j < i+kObfuscateXORKeyLength && j < obfsPartLength; j++ {
 			packet.Data[j] ^= xorKey[j-i]
 		}
 	}
@@ -238,30 +244,9 @@ func (o *WireGuardObfuscator) ReadFromUDPWithDeobfuscate(conn *net.UDPConn, pack
 	return
 }
 
-type ModifiedXXHashDigest struct {
-	xxhash.Digest
-}
-
-func (d *ModifiedXXHashDigest) Sum(b []byte) []byte {
-	s := d.Sum64()
-	return append(
-		b,
-		byte(s>>56),
-		byte(s>>48),
-		byte(s>>40),
-		byte(s>>32),
-		byte(s>>24),
-		byte(s>>16),
-		byte(s>>8),
-		byte(s),
-	)
-}
-
-func (d *ModifiedXXHashDigest) Sum64() uint64 {
-	result := d.Digest.Sum64()
-	high32 := uint32(result >> 32)
-	if high32&0xF8FE_FFFF == 0 {
-		result |= 0xD769_F4C2_0000_0000
+func (o *WireGuardObfuscator) modifyHashMaskForWireGuardHeaderConflict(b []byte) {
+	if b[0]&0b11111000 == 0 && b[1]&0b11111110 == 0 {
+		b[0] |= 0b11010111
+		b[1] |= 0b01101001
 	}
-	return result
 }
